46b0ce6eab808a304c8c1d8b3ca02c14bffa43a8
Anne Lorayne Moreira <annelorayne@gmail.com>
2013-10-04 10:32:49 -0300

    Add my docs

---
+++ CarroImportado.java
@@ -0,0 +1,22 @@
+class CarroImportado{
+	private String modelo;
+	private int ano;
+	
+        //Esse eh o meu carro importado :D
+	public String getModeloNovo(){
+		return modelo+" Novo";
+	}	
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+}
+
