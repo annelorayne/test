a27ca3500671ab74fcc302c3c3ecd7882f390d00
Anne Lorayne Moreira <annelorayne@gmail.com>
2013-10-07 14:09:23 -0300

    Feature - like
    Now, i can.

---
+++ like/like.java
@@ -0,0 +1,25 @@
+class like {
+        //O carro
+        private String modelo;
+        private int ano;
+        private String marca;
+
+        public String getModelo() {
+                return modelo;
+        }
+        public void setModelo(String modelo) {
+                this.modelo = modelo;
+        }
+        public int getAno() {
+                return ano;
+        }
+        public void setAno(int ano) {
+                this.ano = ano;
+        }
+        public String getMarca(){
+                return marca;
+        }
+        public void setMarca(String marca){
+                this.marca = marca;
+        }
+}             
